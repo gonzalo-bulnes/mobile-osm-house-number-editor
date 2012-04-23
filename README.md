b888bed5b217ec17ad0c3a788d8f269c8954c09c
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-04-23 16:35:59 -0400

    Basic setup
    Inital setup of the application. Some adjustments were made to fit
    the Ubuntu 11.10 environement.

---
+++ README.md
@@ -0,0 +1,6 @@
+Durion: a sample Ruby on Rails application
+==========================================
+
+This is a sample application based on the
+[*Ruby on Rails Tutorial: Learn Rails by Example*](http://railstutorial.org/)
+by [Michael Hartl](http://michaelhartl.com/).
--- config/boot.rb
+++ config/boot.rb
@@ -1,5 +1,10 @@
 require 'rubygems'
 
+# Fix JS runtime error on Ubuntu 11.10
+# http://npenkov.com/2011/11/08/get-ruby-on-rails-working-on-ubuntu-11-10/
+require 'execjs'
+require 'v8'
+
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
 
