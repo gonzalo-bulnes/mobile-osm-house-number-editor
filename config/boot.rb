f5522fcd88edf9b3a51148d7bc5d6725e9e60c5b
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-04-25 08:16:15 -0400

    Fix JS runtime error when running rspec
    This error had been fixed by adding 'therubyracer' and 'v8' to the
    config/boot.rb dependencies. That worked fine to run rails server.
    However, it did not prevent the error when running rspec.
    
    Installing node.js solved that; the config/boot.eb dependencies are
    no longer required.
    https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager

--- config/boot.rb
+++ config/boot.rb
@@ -2,8 +2,12 @@ require 'rubygems'
 
 # Fix JS runtime error on Ubuntu 11.10
 # http://npenkov.com/2011/11/08/get-ruby-on-rails-working-on-ubuntu-11-10/
-require 'execjs'
-require 'v8'
+#require 'execjs'
+#require 'v8'
+# The fix before was fine to run rails server, yet running rspec still
+# was impossible (JS runtime error).
+# Installing Node.js solved that:
+# https://github.com/joyent/node/wiki/Installing-Node.js-via-package-manager
 
 # Set up gems listed in the Gemfile.
 ENV['BUNDLE_GEMFILE'] ||= File.expand_path('../../Gemfile', __FILE__)
