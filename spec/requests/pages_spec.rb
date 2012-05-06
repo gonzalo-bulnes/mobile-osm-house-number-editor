e2b848bfefbb601231371c0a41c3a3d43def11d5
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-05-06 16:20:09 -0400

    Refactor Pages layout

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -4,8 +4,18 @@ describe "Pages" do
   describe "Article page" do
     it "should have h1 with 'Article Example'" do
       visit '/article'
-#      response.status.should be(200)
       page.should have_selector('h1', :text => 'Article Example')
     end
+    it "should have basic structure" do
+      visit "/article"
+      page.should have_selector('nav')
+      page.should have_selector('article')
+      page.should have_selector('footer')
+    end
+    it "should have a tags section" do
+      visit "/article"
+      page.should have_selector('aside.tags')
+    end
   end
+  
 end
