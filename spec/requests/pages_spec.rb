f36545b1dc3f9649717d4e7902a153576956b5e7
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-05-07 11:01:44 -0400

    Refactor DRY tests

--- spec/requests/pages_spec.rb
+++ spec/requests/pages_spec.rb
@@ -2,20 +2,14 @@ require 'spec_helper'
 
 describe "Pages" do
   describe "Article page" do
-    it "should have h1 with 'Article Example'" do
-      visit article_path
-      page.should have_selector('h1', :text => 'Article Example')
-    end
+    before { visit article_path }
+
+    it { page.should have_selector('h1', :text => 'Article Example') }
     it "should have basic structure" do
-      visit article_path
       page.should have_selector('nav')
       page.should have_selector('article')
       page.should have_selector('footer')
     end
-    it "should have a tags section" do
-      visit article_path
-      page.should have_selector('aside.tags')
-    end
+    it { page.should have_selector('aside.tags') }
   end
-  
 end
--- spec/requests/static_pages_spec.rb
+++ spec/requests/static_pages_spec.rb
@@ -3,50 +3,23 @@ require 'spec_helper'
 describe "Static pages" do
 
   describe "Code page" do
+    before { visit code_path }
 
-    it "should have the content 'StaticPages#code'" do
-      visit code_path
-      page.should have_content('StaticPages#code')
-    end
-
-    it "should have 'Durion - Code' as title" do
-      visit code_path
-      page.should have_selector('title', :text => 'Durion - Code')
-    end
-
+    it { page.should have_content('StaticPages#code') }
+    it { page.should have_selector('title', :text => 'Durion - Code') }
     it "should have basic CSS selectors" do
-      visit code_path
       page.should have_selector('body#code')
       page.should have_selector('div#page-wrapper')
     end
   end
 
   describe "Talks page" do
+    before { visit talks_path }
 
-    it "should be the root page" do
-      visit root_path
-      page.should have_selector('h1', :text => 'StaticPages#talks')
-    end
-
-    it "should have the content 'StaticPages#talks'" do
-      visit talks_path
-      page.should have_content('StaticPages#talks')
-    end
-
-    it "should have the base title 'Durion'" do
-      visit talks_path
-      page.should have_selector('title', :text => 'Durion')
-    end
-
-    it "should not have a custom title" do
-      visit talks_path
-      page.should_not have_selector('title', :text => 'Talks')
-    end
-
-    it "should have 'talks' as page ID" do
-      visit talks_path
-      page.should have_selector('body#talks')
-    end
-
+    it { page.should have_selector('h1', :text => 'StaticPages#talks') }
+    it { page.should have_content('StaticPages#talks') }
+    it { page.should have_selector('title', :text => 'Durion') }
+    it { page.should_not have_selector('title', :text => 'Talks') }
+    it { page.should have_selector('body#talks') }
   end
 end
