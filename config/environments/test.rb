3493d3a1c26157f7ca82e62f654deb928ed40823
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-06-03 06:39:14 -0400

    Generate devise:install and configure mailer

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -28,6 +28,8 @@ Mobileosmhousenumbereditor::Application.configure do
   # The :test delivery method accumulates sent emails in the
   # ActionMailer::Base.deliveries array.
   config.action_mailer.delivery_method = :test
+  # ActionMailer config
+  config.action_mailer.default_url_options = { :host => 'example.com' }
 
   # Raise exception on mass assignment protection for Active Record models
   config.active_record.mass_assignment_sanitizer = :strict
