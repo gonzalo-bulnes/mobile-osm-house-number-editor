cc625a9aa4f5b1ce0db2f1bbcc87438e8727b26a
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-06-03 06:23:38 -0400

    Fix db/schema.rb error: legacy users table

--- db/schema.rb
+++ db/schema.rb
@@ -22,14 +22,4 @@ ActiveRecord::Schema.define(:version => 20120516135852) do
     t.datetime "updated_at", :null => false
   end
 
-  create_table "users", :force => true do |t|
-    t.string   "name"
-    t.string   "email"
-    t.datetime "created_at",      :null => false
-    t.datetime "updated_at",      :null => false
-    t.string   "password_digest"
-  end
-
-  add_index "users", ["email"], :name => "index_users_on_email", :unique => true
-
 end
