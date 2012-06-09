5f29c7ee441136eae91679f0870341f492848ac4
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-06-09 15:33:46 -0400

    [SI 30954215] Add app revision number to footer
    See http://blog.danielpietzsch.com/post/1209091430/show-the-version-number-of-your-rails-app-using-git#fnref:p1209091430-2

--- config/environment.rb
+++ config/environment.rb
@@ -3,3 +3,7 @@ require File.expand_path('../application', __FILE__)
 
 # Initialize the rails application
 Mobileosmhousenumbereditor::Application.initialize!
+
+# Get a revision number to display if required
+# See http://blog.danielpietzsch.com/post/1209091430/show-the-version-number-of-your-rails-app-using-git
+APP_VERSION = `git describe --always` unless defined? APP_VERSION
