46c735585ada3cbc0acf99fe26d80e675cbf8fd6
Gonzalo Bulnes Guilpain <gon.bulnes@gmail.com>
2012-06-09 10:31:47 -0400

    [SI 30442227] Set assets pre-compilation up
    All CSS files should be renamed with .css.scss extension or
    be included in the config/production.rb precompilation directive.
    
    See https://github.com/rails/rails/issues/4084 and
    https://github.com/rails/sass-rails/issues/81
    
    See http://blog.55minutes.com/2012/01/getting-compass-to-work-with-rails-31-and-32/

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -48,6 +48,10 @@ Mobileosmhousenumbereditor::Application.configure do
   # Precompile additional assets (application.js, application.css, and all non-JS/CSS are already added)
   # config.assets.precompile += %w( search.js )
 
+  # Precompile *all* assets, except those that start with underscore
+  # See http://blog.55minutes.com/2012/01/getting-compass-to-work-with-rails-31-and-32/
+  config.assets.precompile << /(^[^_\/]|\/[^_])[^\/]*$/
+
   # Disable delivery errors, bad email addresses will be ignored
   # config.action_mailer.raise_delivery_errors = false
 
